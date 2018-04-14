6518bc0b95f030f53ec76adce22409850684f9d1
jumdtw <ttnmrm1m3k5g1m3r3k001@ezweb.ne.jp>
2018-04-15 01:09:45 +0900

    list test

--- Matplotlib_labview.py
+++ Matplotlib_labview.py
@@ -1,9 +1,14 @@
 import sys
 
+#データ名を引数に取り、各datafileを読み込む。
 data = sys.argv
-file = 'labview.lvm/' + data[1]
+file = '../labview.lvm/' + data[1]
 with open(file,'r') as f:
     datas = f.read()
-
-
-print(datas)
+'''
+time = []
+vol = []
+i = 0
+while i == 20 :
+'''
+print(datas[1])
